b9a1f3db16390a6278955c5cdbe27de897e25c04
Evgeniy28 <azarov91@mail.ru>
2017-04-02 19:07:39 +0300

    USER ADD TODO, without redux

---
+++ src/components/ui/input/index.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+
+export default class Input extends Component {
+    static propTypes = {
+        value: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired,
+        divClasses: PropTypes.string,
+        error: PropTypes.string
+    };
+
+    constructor(props) {
+        super(props);
+
+        const { value } = this.props;
+        this.state = { value };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const { value } = e.target;
+        this.props.onChange(value);
+        this.setState({ value });
+    }
+
+    render() {
+        const divClasses = classnames({
+            'form-group': true,
+            'has-error': this.props.error ? true : false
+        });
+
+        const { value } = this.props;
+        console.log('todoname', value);
+
+        return (
+            <div className={ divClasses }>
+                <input
+                    className='form-control'
+                    type='text'
+                    value={ value }
+                    onChange={ this.handleChange }
+                />
+                { this.props.error ? <span className='help-block'>{ this.props.error }</span> : null }
+            </div>
+        );
+    }
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
+import { bindAll } from 'lodash';
+
+import Input from '../../components/ui/input/index';
 
 export default class HomePage extends Component {
     static path = '/';
 
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoName: '',
+            todos: [
+                {
+                    id: 1,
+                    name: 'Todo 1'
+                }
+            ],
+            error: ''
+        };
+
+        bindAll(this, ['renderTodos', 'inputOnChange', 'addTodo']);
+    }
+
+    inputOnChange(value) {
+        this.setState({ todoName: value });
+    }
+
+    addTodo() {
+        if (this.state.todoName === '') {
+            this.setState({ error: 'The field can not be empty!' });
+            return;
+        }
+
+        const id = this.state.todos[this.state.todos.length - 1].id + 1;
+        const name = this.state.todoName;
+
+        const todos = this.state.todos;
+        todos.push({ id, name });
+
+        this.setState({ todos });
+        this.setState({ todoName: '', error: '' });
+    }
+
+    renderTodos(item, index) {
         return (
-            <div>
-                <div className="alert alert-success">Hi from Home</div>
+            <li key={ index }>{ item.name }</li>
+        );
+    }
+
+    render() {
+        const { todoName, todos, error } = this.state;
+
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-xs-12'>
+                        <ul>
+                            { todos.map(this.renderTodos) }
+                        </ul>
+                        <Input
+                            onChange={ this.inputOnChange }
+                            value={ todoName }
+                            error={ error }
+                        />
+                        <button className='btn btn-primary' onClick={ this.addTodo }>Add todo</button>
+                    </div>
+                </div>
             </div>
         );
     }
