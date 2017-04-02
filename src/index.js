5d84a04c9fff6da91d3e7368cf1e29620d2417c9
Evgeniy28 <azarov91@mail.ru>
2017-04-02 04:59:59 +0300

    add base settings for redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hi from React!!!');
+import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import configureStore from './store/configureStore';
+import routes from './routes';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render((
+    <Provider store={store}>
+        <Router history={history}>
+            { routes }
+        </Router>
+    </Provider>),
+    document.getElementById('root')
+);
+
