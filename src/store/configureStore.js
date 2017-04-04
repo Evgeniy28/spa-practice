557f485a4c27155ee4f74a8e5cf3e816472586f3
Evgeniy28 <azarov91@mail.ru>
2017-04-05 00:18:32 +0300

    add config for HMR

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -19,5 +19,12 @@ export default function configureStore(initialState) {
         DevTools.instrument()
     )(createStore)(rootReducer, initialState);
 
+    if (module.hot) {
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers');
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
     return store;
 }
